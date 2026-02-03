9172e14bddb85df9a7b75178e0c5ea14d951cf99
Asif Bin Karim <asif.mte.148004@gmail.com>
2026-02-03 12:05:50 -0600

    Vibrotactile feddback added while ring touched the buzz wire

--- Assets/ScriptsNew/collisionChangeRing.cs
+++ Assets/ScriptsNew/collisionChangeRing.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SG;
+using SGCore.Haptics;
 
 public class collisionChangeRing : MonoBehaviour
 {
+    [Header("Wrist Vibration")]
+    public SG_TrackedHand leftHand;
+    public SG_TrackedHand rightHand;
+    [Range(0f, 1f)]
+    public float vibrationIntensity = 1.0f;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "wire")
@@ -17,6 +24,7 @@ public class collisionChangeRing : MonoBehaviour
         if (other.gameObject.tag == "wire")
         {
             GetComponent<MeshRenderer>().material.color = Color.black;
+            TriggerWristVibration();
         }
     }
 
@@ -25,6 +33,19 @@ public class collisionChangeRing : MonoBehaviour
         if (other.gameObject.tag == "wire")
         {
             GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+
+    private void TriggerWristVibration()
+    {
+        if (leftHand != null)
+        {
+            leftHand.SendImpactVibration(SG_HandSection.Wrist, vibrationIntensity);
+        }
+
+        if (rightHand != null)
+        {
+            rightHand.SendImpactVibration(SG_HandSection.Wrist, vibrationIntensity);
         }
     }
 
